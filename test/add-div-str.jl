f3aae98ef9b4283265ef72ec0f4dbd362ad29086
Sole <30993566+sonosole@users.noreply.github.com>
2026-08-12 21:11:41 +0800

    Update add-div-str.jl
    make unique suffix or prefix with 'a':'z' and 'A':'Z', so test would not fail

--- test/add-div-str.jl
+++ test/add-div-str.jl
@@ -3,22 +3,22 @@
        its crucial to make the test passed with no error =#
 
     let PREFIX  = string(rand(UInt32)),
-        SUFFIX1 = reduce(*, rand('A':'Z', 5)),
+        SUFFIX1 = reduce(*, rand('a':'z', 5)),
         SUFFIX2 = reduce(*, rand('A':'Z', 5))
         S1 = PREFIX * SUFFIX1
         S2 = PREFIX * SUFFIX2
         
         @test lcprefix(S1, S2) == PREFIX  # +
-        @test deprefix(S1, S2) == SUFFIX1 # /
+        @test deprefix(S1, S2) == SUFFIX1 # -
     end
 
     let SUFFIX  = string(rand(UInt32)),
-        PREFIX1 = reduce(*, rand('A':'Z', 5)),
+        PREFIX1 = reduce(*, rand('a':'z', 5)),
         PREFIX2 = reduce(*, rand('A':'Z', 5))
         S1 = PREFIX1 * SUFFIX
         S2 = PREFIX2 * SUFFIX
         
         @test lcsuffix(S1, S2) == SUFFIX  # +
-        @test desuffix(S1, S2) == PREFIX1 # /
+        @test desuffix(S1, S2) == PREFIX1 # -
     end
 end
